6d9143a0532bd939f3a9dddfca5e2f63626f1584
RayMetz100 <RayMetz100@hotmail.com>
2015-06-10 07:15:57 -0700

    led program runs after new build

--- DevelopmentCode/TextWalkieTalkie/myProgram/main.c
+++ DevelopmentCode/TextWalkieTalkie/myProgram/main.c
@@ -38,24 +38,24 @@
 
 /*..........................................................................*/
 int main() {
-    //static QEvt const *l_blinkyQSto[10]; /* storage for Blinky event queue */
+    static QEvt const *l_blinkyQSto[10]; /* storage for Blinky event queue */
 
     QF_init();  /* initialize the framework and the underlying RT kernel */
-    //BSP_init(); /* initialize the Board Support Package */
+    BSP_init(); /* initialize the Board Support Package */
 
     /* publish-subscribe not used, no call to QF_psInit() */
     /* dynamic event allocation not used, no call to QF_poolInit() */
 
     /* instantiate and start the active objects... */
-    //Blinky_ctor();
-//    QACTIVE_START(AO_Blinky,      /* AO pointer to start */
-//                  1U,             /* unique QP priority of the AO */
-//                  l_blinkyQSto,   /* storage for the AO's queue */
-//                  Q_DIM(l_blinkyQSto), /* lenght of the queue [entries] */
-//                  (void *)0,      /* stack storage (not used in QK) */
-//                  0U,             /* stack size [bytes] (not used in QK) */
-//                  (QEvt *)0);     /* initial event (or 0) */
+    Blinky_ctor();
+    QACTIVE_START(AO_Blinky,      /* AO pointer to start */
+                  1U,             /* unique QP priority of the AO */
+                  l_blinkyQSto,   /* storage for the AO's queue */
+                  Q_DIM(l_blinkyQSto), /* lenght of the queue [entries] */
+                  (void *)0,      /* stack storage (not used in QK) */
+                  0U,             /* stack size [bytes] (not used in QK) */
+                  (QEvt *)0);     /* initial event (or 0) */
 
-    return 1;//QF_run(); /* run the QF application */
+    return QF_run(); /* run the QF application */
 }
 
